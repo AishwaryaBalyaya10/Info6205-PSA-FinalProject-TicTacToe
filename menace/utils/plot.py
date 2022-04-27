fe199d0bf5776876e00931dedd5a590c9121e0b9
Shubhang Shah <shubhang34@gmail.com>
2022-04-27 00:00:11 -0400

    bug fixes in plot py

--- menace/utils/plot.py
+++ menace/utils/plot.py
@@ -1,7 +1,8 @@
 from typing import Union
 from menace.utils.ternary import Ternary, gameToTernary, ternaryToXandO
 def plot(game: Union[str, Ternary]) -> str:
-    template = ” {1} | {2} | {3} \n” “---+---+---\n” ” {8} | {0} | {4} \n” “---+---+---\n” ” {7} | {6} | {5} ”
+    template = " {1} | {2} | {3} \n" "---+---+---\n" " {8} | {0} | {4} \n" "---+---+---\n" " {7} | {6} | {5} "
+
     if not isinstance(board, Ternary):
         board = gameToTernary(game)
     else:
@@ -10,23 +11,25 @@ def plot(game: Union[str, Ternary]) -> str:
     return template.format(*xAndo)
 def valuePlotting(game: Union[str, Ternary], values: dict, decimal: bool = True) -> str:
     template = (
-        ” {1} | {2} | {3} \n”
-        “-------+-------+------\n”
-        ” {8} | {0} | {4} \n”
-        “-------+-------+------\n”
-        ” {7} | {6} | {5} \n”
+        " {1} | {2} | {3} \n"
+        "-------+-------+------\n"
+        " {8} | {0} | {4} \n"
+        "-------+-------+------\n"
+        " {7} | {6} | {5} \n"
     )
+
     if not isinstance(game, Ternary):
         board = gameToTernary(game)
     else:
         board = game
     values = []
     for k, a in enumerate(ternaryToXandO(board)):
-        if a != ” “:
-            values.append(f”  {a}  “)
+        if a != " ":
+            values.append(f"  {a}  ")
             continue
         if decimal:
-            values.append(“{0:.3f}“.format(round(values[k], 3)))
+            values.append("{0:.3f}".format(round(values[k], 3)))
         else:
-            values.append(”  {0}  “.format(round(values[k])))
+            values.append("  {0}  ".format(round(values[k])))
+            
     return template.format(*values)
